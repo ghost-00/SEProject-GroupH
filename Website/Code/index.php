7682104c1a312bca230a95750ae10ff26044e952
Zigabe jos <jos.zigabe@uclouvain.student.be>
2015-10-24 19:54:48 +0200

    database 3

--- Website/Code/index.php
+++ Website/Code/index.php
@@ -15,7 +15,6 @@ $app['debug'] = true;
  /*
  *inclusion de module
  */
-
 ////------------------------session---------------------------------------
 $app->register(new Silex\Provider\SessionServiceProvider());
 if(isset($_GET['lang']))
@@ -108,14 +107,20 @@ $app->get('/', function(Application $app) {
 															'lang' => $app["session"]->get('lang')));
   });
 
+/**************************************
+
+    LOGIN
+
+***************************************/
+
 $app->post('login', function(Application $app, Request $req) {
 	global $db;
 	
-	$username = $db->quote($req->request->get("username"));
+	$username = $db->quote($req->request->get("usermail"));
 	$password = $db->quote($req->request->get("password"));
 	
 	
-	$req = $db->query("select * from Users where Username = $username and Userpassword = $password");
+	$req = $db->query("select * from Users where Usermail = $usermail and Userpassword = $password");
 	$req = $req->fetchAll();
 	//var_dump($req);
 	
@@ -125,6 +130,87 @@ $app->post('login', function(Application $app, Request $req) {
 		return "vous etes connecté";
 	}
 });
+
+/*****************************
+
+LOGUP
+
+******************************/
+
+$app->post('confirm-new', function(Application $app, Request $req) {
+	global $db;
+	
+	//get last name
+	$userlastname = $db->quote($req->request->get("userlastname"));
+	//get first name
+	$userfirstname = $db->quote($req->request->get("userfirstname"));
+	//get email 
+	$usermail = $db->quote($req->request->get("usermail"));
+	//get wordpass
+	$userpassword = $db->quote($req->request->get("userpassword"));
+	$confirmpassword = $db->quote($req->request->get("confirmpassword"));
+	//get adress : street, number, boxe, zip, locality
+	$useradress = $db->quote($req->request->get("useradress"));
+	$useradressN = $db->quote($req->request->get("useradressN"));
+	$useradressB = $db->quote($req->request->get("useradressB"));
+	$useradressZ = $db->quote($req->request->get("useradressZ"));
+	$useradressL = $db->quote($req->request->get("useradressL"));
+	//get phone
+	$usercellPhone = $db->quote($req->request->get("usercellPhone"));
+	$userphone = $db->quote($req->request->get("userphone"));
+	//get birthday
+	$userbirthday = $db->quote($req->request->get("userbirthday"));
+	//date of creation 
+	$date = date('Y-m-d', strtotime(date('Y-m-d')));
+	
+	echo $userlastname."<br/>";
+	echo $userfirstname."<br/>";
+	echo $usermail."<br/>";
+	//echo password
+	echo $userpassword."<br/>";
+	echo $confirmpassword."<br/>";
+	//echo adress
+	echo $useradress."<br/>";
+	echo $useradressN."<br/>";
+	echo $useradressB."<br/>";
+	echo $useradressZ."<br/>";
+	echo $useradressL."<br/>";
+	//echo phones
+	echo $usercellPhone."<br/>";
+	echo $userphone."<br/>";
+	
+	echo $userbirthday."<br/>";
+	
+	$req = $db->query("INSERT INTO  `tennis_tournament`.`users` (
+`UserId` ,
+`UserFirstName` ,
+`UserLastName` ,
+`UsersBirthDate` ,
+`UserAddress` ,
+`UserAddressN` ,
+`UserAddressB` ,
+`UserAddressC` ,
+`UserAddressL` ,
+`UserPhone` ,
+`UserMail` ,
+`Username` ,
+`Userpassword` ,
+`UserCreationDate`
+)
+VALUES (
+NULL , $userfirstname , $userlastname , $userbirthday , $useradress , $useradressN , $useradressB , $useradressZ , $useradressL , $userphone ,  $usermail, $usermail,  $userpassword, $date
+)") or die($db->errorInfo()[2]);
+
+	$req = $req->fetchAll();
+	//var_dump($req);
+	
+	if(empty($req)){
+		return "Vous etes enregistré";
+	}else{
+		return "Error";
+	}
+});
+
 
 // On lance l'application
 $app->run();
--- Website/Code/web/script/index.js
+++ Website/Code/web/script/index.js
@@ -4,13 +4,46 @@ $(document).ready(function(){
   $('#form-bg').fadeIn(1000);
   $('#sign-in-form').fadeIn(2000);
 
+  /******************************
+	
+  LOGIN
+  *******************************/
   $('#login').click(function(){
-	$.post("index.php/login", {username: $("#email").val(), password: $("#password").val()}, function(data){
+	$.post("index.php/login", {usernmail: $("#email").val(), password: $("#password").val()}, function(data){
 		
 		$("#message").html(data)
 		
 	});
   });
+  
+  /******************************
+  LOGUP
+  *******************************/
+  $('#confirm-new').click(function(){
+	  //alert($("#UserFirstName").val() + "et" + $("#UserLastName").val());
+	$.post("index.php/confirm-new", {userfirstname: $("#UserFirstName").val(), 
+									 userlastname: $("#UserLastName").val(), 
+									 usermail: $("#UserMail").val(), 
+									 userpassword: $("#UserPassword").val(),
+									 confirmpassword: $("#UserPassword2").val(),
+									 //Adresse : street, number, zip-code
+									 useradress: $("#UserAddress").val(),
+									 useradressN: $("#UserAddressN").val(),
+									 useradressB: $("#UserAddressB").val(),
+									 useradressZ: $("#ZipCode").val(),
+									 useradressL: $("#UserAddressL").val(),
+									 //Phone : GSM and fixe
+									 usercellPhone: $("#UserMobile").val(),
+									 userphone: $("#UserPhone").val(),
+									 userbirthday: $("#birthday").val() 
+									 }, function(data){
+		
+		$("#message2").html(data);
+		
+	});
+  });
+  
+
 	
   $('#new-account').click(function(){
 	$('#sign-in-form').hide();
